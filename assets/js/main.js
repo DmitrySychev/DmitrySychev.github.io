2c33378e60e87e8c2a62098355731fd158deccd8
Dmitry <axiumone@gmail.com>
2020-10-20 17:09:57 -0400

    file structure cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Laura - v2.1.0
-* Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
@@ -49,40 +44,6 @@
     }
   });
 
-  // Mobile Navigation
-  // if ($('.nav-menu').length) {
-  //   var $mobile_nav = $('.nav-menu').clone().prop({
-  //     class: 'mobile-nav d-lg-none'
-  //   });
-  //   $('body').append($mobile_nav);
-  //   $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-  //   $('body').append('<div class="mobile-nav-overly"></div>');
-
-  //   $(document).on('click', '.mobile-nav-toggle', function(e) {
-  //     $('body').toggleClass('mobile-nav-active');
-  //     $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-  //     $('.mobile-nav-overly').toggle();
-  //   });
-
-  //   $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-  //     e.preventDefault();
-  //     $(this).next().slideToggle(300);
-  //     $(this).parent().toggleClass('active');
-  //   });
-
-  //   $(document).click(function(e) {
-  //     var container = $(".mobile-nav, .mobile-nav-toggle");
-  //     if (!container.is(e.target) && container.has(e.target).length === 0) {
-  //       if ($('body').hasClass('mobile-nav-active')) {
-  //         $('body').removeClass('mobile-nav-active');
-  //         $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-  //         $('.mobile-nav-overly').fadeOut();
-  //       }
-  //     }
-  //   });
-  // } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-  //   $(".mobile-nav, .mobile-nav-toggle").hide();
-  // }
 
   // Navigation active state on scroll
   var nav_sections = $('section');
@@ -174,11 +135,11 @@
   });
 
   // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+  // $(".portfolio-details-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   items: 1
+  // });
 
 })(jQuery);
